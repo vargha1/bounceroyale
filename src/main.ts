72ef0d389143c56c7b4b6ea7439dc2b06e2d1b5d
vargha <vargha.rahavi@gmail.com>
2025-12-03 14:25:50 +0330

    host

--- src/main.ts
+++ src/main.ts
@@ -1722,7 +1722,7 @@ function init(): void {
 
 window.start = function start(gameMode: string, ip?: string, timer?: number, selectedGameId?: string): void {
   mode = gameMode;
-  serverIp = ip || null;
+  serverIp = ip || "game.safahanbattery.ir";
   startTimer = timer || 30;
   gameId = selectedGameId || null;
 
